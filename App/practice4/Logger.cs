8f2f4a7277fbb3f49ae5e559885950b0ed5472e7
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2025-05-05 13:57:22 +0700

    p4.2

--- App/practice4/Logger.cs
+++ App/practice4/Logger.cs
@@ -4,8 +4,8 @@ public class Logger
 {
     public string logGeometry(IGeometry geometry)
     {
-        if (geometry is Rectangle) return "Прямоугольник";
         if (geometry is Square) return "Квадрат";
+        if (geometry is Rectangle) return "Прямоугольник";
         if (geometry is Triangle) return "Треугольник";
         return "неизвестная геометрия";
     }
--- App/practice4/MyVertex.cs
+++ App/practice4/MyVertex.cs
@@ -5,6 +5,11 @@ public class MyVertex : I2DVertex
     public double X { get; set; }
     public double Y { get; set; }
 
+    public MyVertex(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
     public override String ToString()
     {
         return "(x = " + X + ", y = " + Y + ")";
