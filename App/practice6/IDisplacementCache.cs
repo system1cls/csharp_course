2da8eae510623bbe4f482430ba603f30e5401361
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2025-05-13 11:50:06 +0700

    p6.1

---
+++ App/practice6/IDisplacementCache.cs
@@ -0,0 +1,13 @@
+﻿namespace App.practice6;
+
+public interface IDisplacementCache<T>
+{
+    int Capacity { get; set; }
+    TimeSpan Ttl { get; set; }
+    ITimeService TimeService { get; }
+
+    IReadOnlyDictionary<string, T> GetAllCacheItems();
+    void AddOrUpdate(string key, T item);
+    T TryGet(string key);
+    void ClearExpiredItems();
+}
